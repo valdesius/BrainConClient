629380de997fe416efb8b7937d1e4b7882f513a6
valdesius <v.inc@inbox.ru>
2024-06-20 16:43:28 +0300

    v1.06 fix problem with size

--- app/src/main/java/com/example/brainconclient/ProfileActivity.java
+++ app/src/main/java/com/example/brainconclient/ProfileActivity.java
@@ -22,12 +22,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.example.brainconclient.helpers.ApiLinksHelper;
 import com.example.brainconclient.helpers.RedirectHelper;
 import com.example.brainconclient.helpers.StringResourceHelper;
 import com.example.brainconclient.utils.MyVolleySingletonUtil;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,7 +40,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
     private RequestQueue requestQueue;
-    private TextInputEditText txtProfFirstName, txtProfLastName, txtProfEmail, txtProfPass;
+    private TextInputEditText txtProfFirstName, txtProfLastName, txtProfEmail;
     private Button profileUpdateBtn, profileLogoutBtn;
     ActionBar actionBar;
 
@@ -54,10 +58,8 @@ public class ProfileActivity extends AppCompatActivity {
         txtProfFirstName = findViewById(R.id.txt_prof_first_name);
         txtProfLastName = findViewById(R.id.txt_prof_last_name);
         txtProfEmail = findViewById(R.id.txt_prof_email);
-        txtProfPass = findViewById(R.id.txt_prof_password);
 
         profileUpdateBtn = findViewById(R.id.profile_update_btn);
-        profileLogoutBtn = findViewById(R.id.profile_logout_btn);
 
         profileUpdateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,72 +67,65 @@ public class ProfileActivity extends AppCompatActivity {
                 updateProfile();
             }
         });
-        profileLogoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logUserOut();
-            }
-        });
+
         // END OF PROFILE LOGOUT ON CLICK LISTENER OBJECT.
 
         setUserDetailsInEditTextFields();
 
     }
     public void updateProfile() {
-        // GET DATA:
-        String first_name = txtProfFirstName.getText().toString();
-        String last_name = txtProfLastName.getText().toString();
-        String email = txtProfEmail.getText().toString();
-        String password = txtProfPass.getText().toString();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.PATCH, ApiLinksHelper.updateUserApiUri(), new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.i("ProfileActivity", response.toString());
-                Toast.makeText(ProfileActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                // Update local shared preferences
 
 
-                prefs.edit().putString("first_name", first_name).apply();
-                prefs.edit().putString("last_name", last_name).apply();
-                prefs.edit().putString("email", email).apply();
-                prefs.edit().putString("password", password).apply();
-            }
-        }, new Response.ErrorListener() {
+        // Получаем значения из полей ввода:
+        String firstName = txtProfFirstName.getText().toString().trim();
+        String lastName = txtProfLastName.getText().toString().trim();
+        String email = txtProfEmail.getText().toString().trim();
+        // Пароль не отправляем, так как он не обновляется
+
+        // Создаем новый запрос на обновление профиля:
+        StringRequest updateRequest = new StringRequest(Request.Method.PUT, ApiLinksHelper.updateUserApiUri(),
+                response -> {
+                    // Обработка успешного ответа:
+                    Toast.makeText(ProfileActivity.this, "Профиль успешно обновлен", Toast.LENGTH_SHORT).show();
+                    // Можно здесь обновить SharedPreferences, если необходимо
+                },
+                error -> {
+                    // Обработка ошибки:
+                    Toast.makeText(ProfileActivity.this, "Ошибка обновления профиля", Toast.LENGTH_SHORT).show();
+                }) {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-                Log.i("ProfileActivity", "Failed to update profile");
-                Toast.makeText(ProfileActivity.this, "Failed to update profile", Toast.LENGTH_LONG).show();
-            }
-        }) {
-            @Nullable
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("first_name", first_name);
-                params.put("last_name", last_name);
+                params.put("first_name", firstName);
+                params.put("last_name", lastName);
                 params.put("email", email);
-                params.put("password", password);
-
                 return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                // Добавьте заголовки, если это необходимо, например, токен авторизации:
+                headers.put("Authorization", "Bearer " + prefs.getString("token", ""));
+                return headers;
             }
         };
 
-        // ADD / SEND REQUEST:
-        requestQueue.add(stringRequest);
+        // Добавляем запрос в очередь запросов:
+        requestQueue.add(updateRequest);
     }
-
     // END OF ON CREATE METHOD.
 
     public void setUserDetailsInEditTextFields() {
         // GET STORED PREFERENCES:
         prefs = getSharedPreferences(StringResourceHelper.getUserDetailPrefName(), MODE_PRIVATE);
         // SET TEXT INPUT FIELDS VALUES:
+
+        int userId = prefs.getInt("user_id", 2); // 2 - это значение по умолчанию, если user_id не найден
+
         txtProfFirstName.setText(prefs.getString("first_name", ""));
         txtProfLastName.setText(prefs.getString("last_name", ""));
         txtProfEmail.setText(prefs.getString("username", ""));
-        txtProfPass.setText(prefs.getString("password", ""));
     }
     // END OF SET USER DETAILS IN EDIT TEXT FIELDS.
 
